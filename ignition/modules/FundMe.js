6d8f971e9f3bf178eccad4054eb1b5df23acde75
harpreet singh <happybiostockcode07040@gmail.com>
2024-04-07 17:02:45 +0530

    Hardhat-FundMe-Full-Stack

---
+++ ignition/modules/FundMe.js
@@ -0,0 +1,13 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const priceFeed = "0x5fb1616F78dA7aFC9FF79e0371741a747D2a7F22";
+// const ONE_GWEI = 1_000_000_000n;
+
+module.exports = buildModule("FundMeModule", (m) => {
+  const pricefeed = m.getParameter("priceFeed", priceFeed);
+  // const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  const fundme = m.contract("FundMe", [pricefeed]);
+
+  return { fundme };
+});
